900c6de3d9088da65f50c50c96003b0152f60864
Dn Vidya <anish@ferventsoft.com>
2020-02-26 17:12:48 +0530

    EV-48  Miscellanious changes in hamberg menu has been changed as per client docs #time 40m

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -23,21 +23,22 @@ class MenuComponent extends Component {
 				<li class="nav-item">
 					<a class="nav-link" href="#"><i class="fas fa-lightbulb"></i> How it works</a>
 				</li>
-				<li class="nav-item">
+				{/* <li class="nav-item">
 					<a class="nav-link gray" href="#"><i>&nbsp;</i> Support</a>
-				</li>
+				</li> */}
 				<li class="nav-item">
 					<a class="nav-link" href="#"><i><img src={aboutusIcon} alt=""/></i> About Us</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#"><i class="fas fa-users"></i> Social Media</a>
+					<a class="nav-link gray" href="#"><i>&nbsp;</i> Resources</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><i class="fas fa-users"></i> Help Center</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="#"><i class="fas fa-headset"></i> Contact Us</a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link gray" href="#"><i>&nbsp;</i> Resources</a>
-				</li>
+				
 				{(window.location.pathname.indexOf('/profile')>-1) || (window.location.pathname.indexOf('/admin')>-1) ||
 				<div>
 				<li class="nav-item">
