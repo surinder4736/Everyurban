908f8e7234b2ed33063339b86eede04fd131a837
Dn Vidya <anish@ferventsoft.com>
2020-03-06 18:45:15 +0530

    EV-52 #comment add the search keyword bar in help center page #time 1h

--- src/components/HelpCenter.js
+++ src/components/HelpCenter.js
@@ -10,10 +10,33 @@ export class HelpCenter extends Component {
 
   constructor(props) {
     super(props);
+    this.state={
+      keywords:'',
+      errorMessage:'',
+      initialCollapse:[{
+      title:'Item',
+      body:` Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+      }]
+    }
     
 }
 componentDidMount() {
   
+}
+
+txtKeywordHandleChange(e){
+e.preventDefault();
+this.setState({ keywords:e.target.value,errorMessage:'' });
+}
+
+handleClickSearch(e){
+e.preventDefault();
+if(this.state.keywords=="" && this.state.keywords<=0){
+  this.setState({ errorMessage:'Please enter your keywords'});
+}
+ // const{keywords,initialCollapse}=this.state;
+// let data = initialCollapse.filter(dt=>dt.title==keywords && dt.body==keywords);
+//alert(data[0].title);
 }
 
   render () {
@@ -26,6 +49,23 @@ componentDidMount() {
         <div className="titles" >
             <h1>Help Center</h1><hr></hr>
         </div>
+      <div className="search-bar">
+      <div class="row">
+        <div class="col-12">
+            <div class="input-group">
+                <input class="form-control border-secondary py-2" onChange={this.txtKeywordHandleChange.bind(this)} type="text" placeholder="search keywords here..." />
+                <div class="input-group-append">
+                    <button class="btn btn-primary btn-round-none" type="button" onClick={this.handleClickSearch.bind(this)}>
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+            <div style={{color:'red',fontSize:'14px'}}>{this.state.errorMessage}</div>
+                
+        </div>
+    </div>
+
+      </div>
 <div id="content-wrapper" className="container p-0 content-wrapper">
 
 <div id="accordion">
@@ -38,15 +78,15 @@ componentDidMount() {
     <div class="card-header" id={headingId}>
     <h5 className='arrow-icon'>   
         <a role="button" data-toggle="collapse" href={"#"+collapseId} aria-expanded="false" aria-controls={collapseId}>
-          Item {item}
+         
+          {this.state.initialCollapse.map(itm=>itm.title)} {item}
         </a>
     </h5> 
     </div>
     <div id={collapseId} class={item==1 ? "collapse": "collapse hide"} data-parent="#accordion" aria-labelledby={headingId}>
       <div class="card-body">
         <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          {this.state.initialCollapse.map(dt=>dt.body)}
         </p>
       </div>
     </div>
