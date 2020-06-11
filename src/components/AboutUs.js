c9347d0efe905b3dc5bb455a33b1b883dbca04b7
Surinder Kumar <Surinder@ferventsoft.com>
2020-06-11 14:06:21 +0530

    changes for about us page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,6 +9,9 @@ import MenuComponent from './MenuComponent';
 // import subscribe from '../Images/subscribe-1.jpg';
 import footerLogo from '../Images/logo-footer.png';
 import Footer from './Footer';
+import Community from '../Images/Community.png';
+import Protecting from '../Images/Protecting.png';
+import Empowering from '../Images/Empowering.png';
 export class AboutUs extends Component {
  // static displayName = Home.name;
 
@@ -28,19 +31,56 @@ componentDidMount() {
 				{/* Slider */}
 		    <section id="aboutus" >
           <div className="aboutus-body">
-            <h1>Our Mission</h1>
-            <p>
+            <h1>About Us</h1>
+            {/* <p>
               Use place holder for now content is available. Should be around two or three lines,not too long. Longer story will be below.  
               The menu bar rather than "About Us" will be changed to "Our Mission".
+            </p> */}
+            <p>
+            EveryUrban was created in order to help propel the world of planning, design, and development towards the future through adopting technologies and strategies to create the best sustainable communities. We aim to empower creativity by offering open opportunities to designers from all backgrounds and skillsets around the world.
+            </p>
+            <p>
+            Architectural designers can more closely connect with their core passions and freely create without limiting imagination to a strict agenda while gaining experience and income opportunities to further their careers.
+            </p>
+            <p>
+            Builders can discover new talent and ideas that would normally be confined to the local environment with added support and professional guidance from the EveryUrban expert team.
+            </p>
+            <p>
+            EveryUrban helps bridge the gap in communication between architectural design and development by bringing additional expertise to maximize project vision and success, streamlining the process of strategy, design and negotiation.
             </p>
           </div>
           <div className="container aboutus-cotent">
-            <p style={{color:'#3AA3d7'}}>
-               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
+            <h1 className="text-center" style={{color:'#3AA3d7'}}>Our Key Objectives:</h1><br/>
+            <div className="row" style={{color:'#3AA3d7'}}>
+              <div className="col-md-4">
+                <p className="text-center" style={{fontWeight:'600',fontSize:'20px'}}>Community Wellness</p><br/>
+                <div className="text-center">
+                <img src={Community} alt="" width="50%"/>
+                </div>
+                <p className="text-center">
+                Incorporating smart features into spaces to improve convenience, reducing stress, and strengthen community relationships within the neighborhood and local city planners.
+                </p>
+              </div>
+              <div className="col-md-4">
+                <p className="text-center" style={{fontWeight:'600',fontSize:'20px'}}>Protecting Our Planet</p><br/>
+                <div className="text-center">
+                <img src={Protecting} alt="" width="50%"/>
+                </div>
+                <p className="text-center">High-efficiency systems and materials managing consumption and reducing waste whilst maximizing yield. We want to build harmoniously with the surrounding environment.</p>
+              </div>
+              <div className="col-md-4">
+                <p className="text-center" style={{fontWeight:'600',fontSize:'20px'}}>Empowering the Economy</p><br/>
+                <div className="text-center">
+                  <img src={Empowering} alt="" width="50%"/>
+                  </div>
+                <p className="text-center">
+                Maximizing performance, capacity, and consolidating the need for housing will help intensify growing regions, strengthen the economy, and promote growth creating further opportunities for the local community.
+                </p>
+              </div>
+            </div>
+            
           </div>
+          
 			  </section>
       <Footer />
       </div>
