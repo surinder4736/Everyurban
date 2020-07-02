1eaa795ef3498bc0e6a9e99dfc12179c15ec446e
Surinder Kumar <Surinder@ferventsoft.com>
2020-07-02 15:09:50 +0530

    reduce size how it works and change ip with domain

--- src/Config/config.production.js
+++ src/Config/config.production.js
@@ -1,7 +1,7 @@
 export const ENV ='production';
-export const BASE_URL = 'http://122.160.166.186:9084';
+export const BASE_URL = `http://everyurban.com;` ///'http://45.62.223.40:8081';
 export const APIURL = `${BASE_URL}/api/`;
-export const hostName = `http://122.160.166.186:9085`;
+export const hostName = `http://everyurban.com`;
 export const checkAppHostName = hostName;
 
 
