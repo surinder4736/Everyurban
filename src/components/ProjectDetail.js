ae3d8ccd0f2c851872691700e980d2e26fba983d
Surinder Kumar <Surinder@ferventsoft.com>
2020-11-03 12:45:06 +0530

    fix projectdetail pge issue

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -35,16 +35,16 @@ export class ProjectDetail extends Component {
             <div>
                 <Header />
                 <MetaTags>                    
-                    <meta name="title" content={`${blog_detail.blog.postmetaextension}`} />
-                    <meta name="description" content={`${blog_detail.blog.postmetaextension}`}/>
-                    <meta name="Publisher" content={`${blog_detail.blog.publisher}`} />
-                    <meta name="Keywords" content={`${blog_detail.blog.keywords}`}/>
+                    <meta name="title" content={`${blog_detail.blog!=undefined?blog_detail.blog.postmetaextension:''}`} />
+                    <meta name="description" content={`${blog_detail.blog!=undefined?blog_detail.blog.postmetaextension:''}`}/>
+                    <meta name="Publisher" content={`${blog_detail.blog!=undefined?blog_detail.blog.publisher:''}`} />
+                    <meta name="Keywords" content={`${blog_detail.blog!=undefined?blog_detail.blog.keywords:''}`}/>
                 </MetaTags>
                 <MenuComponent />
                 {/* Slider */}
+                {blog_detail.blog!=null && blog_detail.blog!=undefined &&
                 <section id="howitwork">
-                   
-                    <div className="howitwork-body">
+                   <div className="howitwork-body">
                         <h1>Project RFPs</h1>
                         <p> {blog_detail.blog.posturlextension == this.props.match.params.project_url ?
                             <span className={''} dangerouslySetInnerHTML={{ __html: unescape(blog_detail.blog.posttitle) }}></span>: ""}
@@ -52,14 +52,15 @@ export class ProjectDetail extends Component {
                     </div>
                     <div className="howitwork-content ">
                          <div className="row list-body">
-    <div className="col-lg-12 ">
+                            <div className="col-lg-12 ">
                                 <div className="bs-component">
                                     {blog_detail.blog.posturlextension == this.props.match.params.project_url ? <span className={'postdetails useful-link'} dangerouslySetInnerHTML={{ __html: unescape(blog_detail.blog.postcontent) }}></span> : "No record found"}
                                  </div>
-          </div>
-        </div>
-                        </div>                        
+                            </div>
+                        </div>
+                    </div>                        
                 </section>
+                }
                 <Footer />
             </div>
         )
